6f5d7417edce7c74fd29cdb44b8e51746a77a9b3
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2025-03-20 02:26:47 +0400

    commit

--- Bank-Project/src/components/Home/Home.jsx
+++ Bank-Project/src/components/Home/Home.jsx
@@ -23,20 +23,20 @@ const Home = () => {
 
 
     const setIndividuals = () => {
-        const benefits =[
-            {benefitName:"Checking Accounts",benefit:"Enjoy easy and convenient access to your funds with our range of checking account options. Benefit from features such as online and mobile banking, debit cards, and free ATM access." },
-            {benefitName:"Savings Accounts", benefit:"Build your savings with our competitive interest rates and flexible savings account options. Whether you're saving for a specific goal or want to grow your wealth over time, we have the right account for you."},
-            {benefitName:"Loans and Mortgages", benefit:"Realize your dreams with our flexible loan and mortgage options. From personal loans to home mortgages, our experienced loan officers are here to guide you through the application process and help you secure the funds you need."}
+        const benefits = [
+            { benefitName: "Checking Accounts", benefit: "Enjoy easy and convenient access to your funds with our range of checking account options. Benefit from features such as online and mobile banking, debit cards, and free ATM access.", img: "public/home-images/job.png",id:"job" },
+            { benefitName: "Savings Accounts", benefit: "Build your savings with our competitive interest rates and flexible savings account options. Whether you're saving for a specific goal or want to grow your wealth over time, we have the right account for you.", img: "public/home-images/save.png", id: "save"},
+            { benefitName: "Loans and Mortgages", benefit: "Realize your dreams with our flexible loan and mortgage options. From personal loans to home mortgages, our experienced loan officers are here to guide you through the application process and help you secure the funds you need.", img: "public/home-images/money.png", id:"money"}
         ]
         setIndividualAdvantage(benefits)
-        setBuisnessReview([])
+        setBuisnessAdvantage([])
         setChoice("For-individuals")
     }
     const setBuisness = () => {
         const benefits = [
-            {benefitName:"Cash flow management", benefit:"Efficient cash flow management is vital for the financial health and stability of a business. Banking services, including money management instruments and electronic payments help businesses to optimise their cash flow, oversee liquidity, as well as to sure that there are sufficient funds in the account to fulfil their financial obligations."},
-            { benefitName:"Custom Settings", review:"make your own decisions about the exact feature access a user has." },
-            { benefitName: "Keeping Finances Separate", review:"Maintaining a distinct boundary between personal and professional is analogous to keeping your transactions private. This division guarantees that your purchases, income, and business expenses are tracked independently from your personal spending." }
+            { benefitName: "Cash flow management", benefit: "Efficient cash flow management is vital for the financial health and stability of a business. Banking services, including money management instruments and electronic payments help businesses to optimise their cash flow, oversee liquidity, as well as to sure that there are sufficient funds in the account to fulfil their financial obligations.", img:"public/home-images/money.png", id:"cashFlow" },
+            { benefitName: "Custom Settings", benefit: "make your own decisions about the exact feature access a user has. oversee liquidity, as well as to sure that there are sufficient funds in the account to fulfil their financial obligations.",img:"public/home-images/settings.png", id:"customSettings" },
+            { benefitName: "Keeping Finances Separate", benefit: "Maintaining a distinct boundary between personal and professional is analogous to keeping your transactions private. This division guarantees that your purchases, income, and business expenses are tracked independently from your personal spending.",img:"public/home-images/card.png", id:"seperate" }
         ]
         setChoice("For-Buisness")
         setBuisnessAdvantage(benefits)
@@ -94,8 +94,8 @@ const Home = () => {
 
                         <div id="decision">
                             <span>
-                                <button id="individuals" onClick={setIndividuals} style={review === "For-individuals" ? { backgroundColor: "#caff33", } : { color: "white", background: "transparent" }}>For Individuals</button>
-                                <button id="buisness" onClick={setBuisness} style={review === "For-Buisness" ? { backgroundColor: "#caff33" } : { color: "white", background: "transparent" }}>For Buisness</button>
+                                <button id="individuals" onClick={setIndividuals} style={choice === "For-individuals" ? { backgroundColor: "#caff33", } : { color: "white", background: "transparent" }}>For Individuals</button>
+                                <button id="buisness" onClick={setBuisness} style={choice === "For-Buisness" ? { backgroundColor: "#caff33" } : { color: "white", background: "transparent" }}>For Buisness</button>
                             </span>
 
                         </div>
@@ -103,8 +103,43 @@ const Home = () => {
 
 
                     <div id="benefits">
-
+                        {
+                            choice === "For-individuals" ? (
+                                IndividualAdvantage.map(item => {
+                                    return (
+                                        <div>
+                                            <button id={item.id}>
+                                               <img src={item.img} alt={item.benefitName} />
+                                            </button>
+                                            
+                                            <h2>{item.benefitName}</h2>
+                                            <p>{item.benefit}</p>
+                                        </div>
+                                    )
+                                })
+                            ) : (
+                                buisnessAdvantage.map(item => {
+                                    return (
+                                        <div>
+                                            <button id={item.id}>
+                                               <img src={item.img} alt={item.benefitName} />
+                                            </button>
+                                            <h2>{item.benefitName}</h2>
+                                            <p>{item.benefit}</p>
+                                        </div>
+                                    )
+                                })
+                            )
+                        }
                     </div>
+                </div>
+                <div id="cases">
+                    <span id="case-head">
+                        <h2>
+                            Use Cases
+                        </h2>
+                        <p>At YourBank, we cater to the diverse needs of individuals and businesses alike, offering a wide range of financial solutions</p>
+                    </span>
                 </div>
             </main >
 
